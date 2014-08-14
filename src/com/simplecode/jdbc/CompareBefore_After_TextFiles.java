7e2dc1515dbb8dd9130c1e4fd3f337c2dac8226a
MiraNatalia <miranatalia3@gmail.com>
2014-08-14 19:41:07 +0300

    CompareFilesProgram

--- src/com/simplecode/jdbc/CompareBefore_After_TextFiles.java
+++ src/com/simplecode/jdbc/CompareBefore_After_TextFiles.java
@@ -2,6 +2,8 @@
 package com.simplecode.jdbc;
 
 import java.io.File;
+import java.io.IOException;
+
 import org.apache.commons.io.FileUtils;
 
 public class CompareBefore_After_TextFiles 
@@ -9,8 +11,46 @@ public class CompareBefore_After_TextFiles
         public static void main(String[] args) throws Exception 
         {
         	
-        
+        	File folderBefore = new File("C:\\Natalia\\Work\\NNA ODM Response Reporting\\ToTest\\National\\Before");
+        	File folderAfter = new File("C:\\Natalia\\Work\\NNA ODM Response Reporting\\ToTest\\National\\After");
+            
+        	String beforeDeployment[] = folderBefore.list();
+        	String afterDeployment[] = folderAfter.list();
         	
+        	for (int i = 0; i < beforeDeployment.length; i++) {
+        		
+        		File fileBefore = new File ("C:\\Natalia\\Work\\NNA ODM Response Reporting\\ToTest\\National\\Before\\" + beforeDeployment[i]);
+        		File fileAfter = new File ("C:\\Natalia\\Work\\NNA ODM Response Reporting\\ToTest\\National\\After\\" + afterDeployment[i]);
+        		
+        		compareTextFiles(fileBefore,fileAfter );
+        		
+        	}
+        	     	
                 
         }
+           
+        
+        public  static void  compareTextFiles(File vFileBefore, File vFileAfter) 
+        {
+        	
+        	boolean compareResult = false;
+			try {
+				compareResult = FileUtils.contentEquals(vFileBefore, vFileAfter);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            System.out.println("Are the files are same? " + vFileBefore.getName()+ " vs " + vFileAfter.getName()+ "  " + compareResult);
+                
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 }
