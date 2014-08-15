ceebf0b99e7304cf607f8dda388669e56811c20d
MiraNatalia <miranatalia3@gmail.com>
2014-08-15 19:42:26 +0300

    CompareStrings

--- src/com/simplecode/jdbc/CompareBefore_After_TextFiles.java
+++ src/com/simplecode/jdbc/CompareBefore_After_TextFiles.java
@@ -11,16 +11,16 @@ public class CompareBefore_After_TextFiles
         public static void main(String[] args) throws Exception 
         {
         	
-        	File folderBefore = new File("C:\\Natalia\\Work\\NNA ODM Response Reporting\\ToTest\\National\\Before");
-        	File folderAfter = new File("C:\\Natalia\\Work\\NNA ODM Response Reporting\\ToTest\\National\\After");
+        	File folderBefore = new File("C:\\Totest\\Before");
+        	File folderAfter = new File("C:\\Totest\\After");
             
         	String beforeDeployment[] = folderBefore.list();
         	String afterDeployment[] = folderAfter.list();
         	
         	for (int i = 0; i < beforeDeployment.length; i++) {
         		
-        		File fileBefore = new File ("C:\\Natalia\\Work\\NNA ODM Response Reporting\\ToTest\\National\\Before\\" + beforeDeployment[i]);
-        		File fileAfter = new File ("C:\\Natalia\\Work\\NNA ODM Response Reporting\\ToTest\\National\\After\\" + afterDeployment[i]);
+        		File fileBefore = new File ("C:\\Totest\\Before\\" + beforeDeployment[i]);
+        		File fileAfter = new File ("C:\\Totest\\After\\" + afterDeployment[i]);
         		
         		compareTextFiles(fileBefore,fileAfter );
         		
@@ -34,14 +34,21 @@ public class CompareBefore_After_TextFiles
         {
         	
         	boolean compareResult = false;
-			try {
-				compareResult = FileUtils.contentEquals(vFileBefore, vFileAfter);
-			} catch (IOException e) {
+        	       	
+        	try {
+				String beforeFile = FileUtils.readFileToString(vFileBefore);
+				String afterFile = FileUtils.readFileToString(vFileAfter);
+				compareResult = beforeFile.equals(afterFile);
+				System.out.println(vFileBefore.getName()+ " 555555555555555555555555" + beforeFile + "333");
+				System.out.println(vFileAfter.getName()+ " 555555555555555555555555" + afterFile + "333");
+				System.out.println("Are the files are same? " + vFileBefore.getName()+ " vs " + vFileAfter.getName()+ "  " + compareResult);
+//				System.out.println(vFileBefore.getName()+ " content: " + beforeFile);
+//				System.out.println(vFileAfter.getName()+ " content: " + afterFile);
+			} catch (IOException e1) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				e1.printStackTrace();
 			}
-            System.out.println("Are the files are same? " + vFileBefore.getName()+ " vs " + vFileAfter.getName()+ "  " + compareResult);
-                
+        	               
         }
         
         
