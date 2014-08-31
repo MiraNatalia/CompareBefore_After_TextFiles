36ab5dc427d65924782327544ba309421ff92d4f
MiraNatalia <miranatalia3@gmail.com>
2014-08-31 19:39:45 +0300

    ReadStringsWithoutSpaces

--- src/com/simplecode/jdbc/CompareBefore_After_TextFiles.java
+++ src/com/simplecode/jdbc/CompareBefore_After_TextFiles.java
@@ -1,8 +1,13 @@
 
 package com.simplecode.jdbc;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.commons.io.FileUtils;
 
@@ -22,10 +27,46 @@ public class CompareBefore_After_TextFiles
         		File fileBefore = new File ("C:\\Totest\\Before\\" + beforeDeployment[i]);
         		File fileAfter = new File ("C:\\Totest\\After\\" + afterDeployment[i]);
         		
-        		compareTextFiles(fileBefore,fileAfter );
+        		BufferedReader readerBefore = new BufferedReader(new FileReader(fileBefore));
+        		BufferedReader readerAfter = new BufferedReader(new FileReader(fileAfter));
         		
+        		boolean compare = true;
+        		boolean equals = true;
+        		
+				while (compare){
+        			
+        			String beforeFileString = readerBefore.readLine();
+    				String afterFileString = readerAfter.readLine();
+        			
+    				if(beforeFileString == null && afterFileString != null)
+    				{
+    					//False
+    					compare = false;
+    					equals = false;
+    				}    				
+    				else if(beforeFileString != null && afterFileString == null)
+    				{
+    					//False
+    					compare = false;
+    					equals  = false;
+    				} 
+    				else if (beforeFileString != null && afterFileString != null)
+    				{
+    					equals = compare = beforeFileString.replace(" ", "").equals(afterFileString.replace(" ", ""));
+    				}
+    				else
+    				{
+    					compare = false;// End of bouth file reached
+    				}
+        			       			
+        		}
+				
+				readerBefore.close();
+				readerAfter.close();
+				System.out.println("Are the files are same? " + fileBefore.getName()+ " vs " + fileAfter.getName()+ "  " + equals);
+//				
         	}
-        	     	
+        	     	  
                 
         }
            
@@ -38,11 +79,25 @@ public class CompareBefore_After_TextFiles
         	try {
 				String beforeFile = FileUtils.readFileToString(vFileBefore);
 				String afterFile = FileUtils.readFileToString(vFileAfter);
+				beforeFile = beforeFile.replace(" ", "");
+				afterFile = afterFile.replace(" ", "");
 				compareResult = beforeFile.equals(afterFile);
-				System.out.println(vFileBefore.getName()+ " 555555555555555555555555" + beforeFile + "333");
-				System.out.println(vFileAfter.getName()+ " 555555555555555555555555" + afterFile + "333");
-				System.out.println("Are the files are same? " + vFileBefore.getName()+ " vs " + vFileAfter.getName()+ "  " + compareResult);
-//				System.out.println(vFileBefore.getName()+ " content: " + beforeFile);
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+//				System.out.println(vFileBefore.getName()+ " 555555555555555555555555" + beforeFile + "333");
+//				System.out.println(vFileAfter.getName()+ " 555555555555555555555555" + afterFile + "333");
+			System.out.println("Are the files are same? " + vFileBefore.getName()+ " vs " + vFileAfter.getName()+ "  " + compareResult);
+//			System.out.println(vFileBefore.getName()+ " content: " + beforeFile);
 //				System.out.println(vFileAfter.getName()+ " content: " + afterFile);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
